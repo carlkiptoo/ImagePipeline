6867f36063436c2234c076a14fa5fcd09d393696
carlkiptoo <carlosiamcarl@gmail.com>
2025-09-24 12:19:50 +0300

    Server setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.get('/health', (req, res) => res.json({status: "ok"}));
+
+app.use('/api/upload', uploadRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    
+})
